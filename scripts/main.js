ecb3590a414a9f9a77c9039eabf6ab40eebb1e2e
benshillito20 <72322620+benshillito20@users.noreply.github.com>
2020-10-14 04:24:52 +0100

    main2

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,16 @@
 (function(){
     var hideArrow = document.querySelector('#leftArrow');
-    var transparentOverlay = document.querySelector('#transparentOverlay');
     var navBar = document.querySelector('aside');
     var menuButton = document.querySelector('#menuButton');
 
     hideArrow.addEventListener('click', function(e){
-        transparentOverlay.classList.toggle('transparentOverlayActive');
+        document.getElementById('transparentOverlay').style.display = 'none';
         navBar.classList.toggle('slideOut');
         navBar.classList.toggle('slideIn');
         e.preventDefault();
     })
     menuButton.addEventListener('click', function(e){
-        transparentOverlay.classList.toggle('transparentOverlayActive');
+        document.getElementById('transparentOverlay').style.display = 'block';
         navBar.classList.toggle('slideOut');
         navBar.classList.toggle('slideIn');
         e.preventDefault();
