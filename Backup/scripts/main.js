ea8763d9a895622dffa74015cca7cfefa612263b
benshillito20 <72322620+benshillito20@users.noreply.github.com>
2020-10-14 04:26:24 +0100

    main3

--- Backup/scripts/main.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    var hideArrow = document.querySelector('#leftArrow');
-    var transparentOverlay = document.querySelector('#transparentOverlay');
-
-    hideArrow.addEventListener('click', function(e){
-        transparentOverlay.classList.toggle('transparentOverlayActive');
-        e.preventDefault();
-    })
-})();
